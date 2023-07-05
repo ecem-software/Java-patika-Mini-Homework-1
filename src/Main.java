ed2b0d297ca1380479f8698619076d6d0570e11f
Ecem Terzi <ecemterz@gmail.com>
2023-07-05 18:37:14 +0300

    First Commit in hw

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        List<String> StudentClassA= new ArrayList<>();
+        List<String> StudentClassB= new ArrayList<>();
+        List<String> StudentClassC= new ArrayList<>();
+
+        StudentClassA.add("Murathan Güleç");
+        StudentClassA.add("Serkan Öz");
+        StudentClassA.add("Murathan Güleç");
+        StudentClassA.add("Furkan Demir");
+
+        StudentClassB.add("Mert Uslu");
+        StudentClassB.add("Ahmethan Yılmaz");
+        StudentClassB.add("Emrecan Demir");
+        StudentClassB.add("Handan Kara");
+
+        StudentClassC.add("Leylan Tatlı");
+        StudentClassC.add("Kayra Uçar");
+        StudentClassC.add("Ayla Metehan");
+        StudentClassC.add("Aslı Koçak");
+
+
+
+      Map<String, List<String>> Classstudent = new HashMap<>();
+        Classstudent.put("A",StudentClassA);
+        Classstudent.put("B",StudentClassB);
+        Classstudent.put("C",StudentClassC);
+
+        for (Map.Entry<String, List<String>> entry : Classstudent.entrySet()) {
+            String className = entry.getKey();
+            List<String> students = entry.getValue();
+
+            for (String student : students) {
+                String[] split = student.split(" ");
+                String name = split[0];
+
+                if (name.endsWith("an")) {
+                    System.out.println("class:"+ className + ": " + student);
+                }
+            }
+        }
+    }
+}
+
+
