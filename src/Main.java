2a3c8e4c906b54344aa9a95a5ee137baca68e1e7
Ecem Terzi <ecemterz@gmail.com>
2023-07-05 23:55:15 +0300

    First Commit in hw

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        List<String> StudentClassA= new ArrayList<>();
-        List<String> StudentClassB= new ArrayList<>();
-        List<String> StudentClassC= new ArrayList<>();
+        List<String> StudentClassA = new ArrayList<>();
+        List<String> StudentClassB = new ArrayList<>();
+        List<String> StudentClassC = new ArrayList<>();
 
         StudentClassA.add("Murathan Güleç");
         StudentClassA.add("Serkan Öz");
-        StudentClassA.add("Murathan Güleç");
+        StudentClassA.add("Murathan Güler");
         StudentClassA.add("Furkan Demir");
 
         StudentClassB.add("Mert Uslu");
@@ -26,13 +27,12 @@ public class Main {
         StudentClassC.add("Aslı Koçak");
 
 
+        Map<String, List<String>> classStudent = new HashMap<>();
+        classStudent.put("A", StudentClassA);
+        classStudent.put("B", StudentClassB);
+        classStudent.put("C", StudentClassC);
 
-      Map<String, List<String>> Classstudent = new HashMap<>();
-        Classstudent.put("A",StudentClassA);
-        Classstudent.put("B",StudentClassB);
-        Classstudent.put("C",StudentClassC);
-
-        for (Map.Entry<String, List<String>> entry : Classstudent.entrySet()) {
+        for (Map.Entry<String, List<String>> entry : classStudent.entrySet()) {
             String className = entry.getKey();
             List<String> students = entry.getValue();
 
@@ -41,7 +41,7 @@ public class Main {
                 String name = split[0];
 
                 if (name.endsWith("an")) {
-                    System.out.println("class:"+ className + ": " + student);
+                    System.out.println("class:" + className + ": " + student);
                 }
             }
         }
